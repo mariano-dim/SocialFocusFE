50687c3d904ed5b7185e0029ac9a0b939e74a8f6
damian85 <damian.rivas@gmail.com>
2016-10-05 18:01:21 -0300

    Avances en Frontend - Registro, Login y Botones Redes Sociales

---
+++ app/MarianoBK/models/usuario.ts
@@ -0,0 +1,12 @@
+export interface Usuario {
+    id: string;
+    email: string;
+    clave: string;
+    creationDate: string;
+    telefono: string;
+    fechaNacimiento: string;
+    pais: string;
+    provincia: string;
+    ciudad: string;
+    calle: string;
+}
